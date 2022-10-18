17f2513070dc0cfb1526fd7f4e6d85ffb1e830c6
Alexander Bobryshev <94990187+AlexBob98@users.noreply.github.com>
2022-10-18 21:05:38 +0300

    feat: add index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './assets/styles/style.css';
+import './assets/js/script';
+import './assets/js/animation';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
